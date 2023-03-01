f9c379e310109e12b9e92e71aa5ad4d794d26e1b
wangzhaoyong <wangzhaoyong@huawei.com>
2023-03-01 14:35:11 +0800

    handle exception cherrypick monthly issue: https://gitee.com/openharmony/arkui_napi/issues/I6I8N9
    Signed-off-by: wangzhaoyong <wangzhaoyong@huawei.com>
    Change-Id: I03589d7ae91e36f8353f13a00e0ad2efab8ba6fd

--- native_engine/impl/ark/ark_native_engine_impl.cpp
+++ native_engine/impl/ark/ark_native_engine_impl.cpp
@@ -780,9 +780,10 @@ NativeValue* ArkNativeEngineImpl::GetAndClearLastException(NativeEngine* engine)
 {
     NativeValue* temp = lastException_;
     lastException_ = nullptr;
-    if (IsMainThread()) {
+    if (IsMainThread() || temp != nullptr) {
         return temp;
     }
+
     // Worker need handle all exception
     LocalScope scope(vm_);
     Local<ObjectRef> exception = panda::JSNApi::GetAndClearUncaughtException(vm_);
