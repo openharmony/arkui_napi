2afa71da691921c343274a86ca03fcfdaf72931f
ZhangYu <zhangyu578@huawei.com>
2023-06-12 07:22:53 +0000

    Support advanced components
    Signed-off-by: ZhangYu <zhangyu578@huawei.com>
    Change-Id: I6d841189b4303fdb6595b59fd66204f10a3d7d11

--- module_manager/native_module_manager.cpp
+++ module_manager/native_module_manager.cpp
@@ -430,7 +430,11 @@ bool NativeModuleManager::GetNativeModulePath(const char* moduleName, const char
 #endif
     } else {
         if (relativePath[0]) {
-            sysPrefix = sysPrefix + "/" + relativePath;
+            if (previewSearchPath_.empty()) {
+                sysPrefix = sysPrefix + "/" + relativePath;
+            } else {
+                sysPrefix = previewSearchPath_ + "/module";
+            }
         }
         prefix = sysPrefix.c_str();
         for (int32_t i = 0; i < lengthOfModuleName; i++) {
@@ -700,4 +704,9 @@ int32_t NativeModuleManager::GetProcessExtensionType()
         return EXTENSION_TYPE_UNSPECIFIED;
     }
     return moduleLoadChecker_->GetProcessExtensionType();
+}
+
+void NativeModuleManager::SetPreviewSearchPath(const std::string& previewSearchPath)
+{
+    previewSearchPath_ = previewSearchPath;
 }
