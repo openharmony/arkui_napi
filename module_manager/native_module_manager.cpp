be4db319405d6ffbb8f96b0b6695aa8a7143d424
yinchuang <yinchuang@huawei.com>
2023-06-30 15:41:05 +0800

    Make ndk so accessible to the system so
    Issue:I7HF2Z
    
    Signed-off-by: yinchuang <yinchuang@huawei.com>

--- module_manager/native_module_manager.cpp
+++ module_manager/native_module_manager.cpp
@@ -266,6 +266,7 @@ void NativeModuleManager::CreateLdNamespace(const std::string moduleName, const 
         if (strlen(ndk_ns.name) > 0) {
             dlns_inherit(&ns, &ndk_ns, ALLOW_ALL_SHARED_LIBS);
             dlns_inherit(&ndk_ns, &current_ns, ALLOW_ALL_SHARED_LIBS);
+            dlns_inherit(&current_ns, &ndk_ns, ALLOW_ALL_SHARED_LIBS);
             dlns_inherit(&ns, &current_ns, ALLOW_ALL_SHARED_LIBS);
         }
     } else {
@@ -278,6 +279,7 @@ void NativeModuleManager::CreateLdNamespace(const std::string moduleName, const 
         if (strlen(ndk_ns.name) > 0) {
             dlns_inherit(&ns, &ndk_ns, ALLOW_ALL_SHARED_LIBS);
             dlns_inherit(&ndk_ns, &current_ns, ALLOW_ALL_SHARED_LIBS);
+            dlns_inherit(&current_ns, &ndk_ns, ALLOW_ALL_SHARED_LIBS);
         }
     }
 
