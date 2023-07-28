852e902890d270f76035439bede5dc733ce0d395
shegangbin <shegangbin1@huawei.com>
2023-07-28 19:40:59 +0800

    fixed 57394d0 from https://gitee.com/vulkansgb/arkui_napi/pulls/877 add libnative_buffer.so to arkui_napi
    Signed-off-by: shegangbin <shegangbin1@huawei.com>

--- module_manager/native_module_manager.cpp
+++ module_manager/native_module_manager.cpp
@@ -252,6 +252,7 @@ void NativeModuleManager::CreateSharedLibsSonames()
         "libhukssdk.z.so",
         "libnative_drawing.so",
         "libnative_window.so",
+        "libnative_buffer.so",
         "libnative_vsync.so",
         "libOpenSLES.so",
         "libpixelmap_ndk.z.so",
