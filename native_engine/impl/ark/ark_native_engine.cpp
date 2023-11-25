d778943610e7d340a60f3722483f8d104c1be4a9
Li <xylika@isoftstone.com>
2023-11-25 16:33:26 +0800

    Resolve test_ concurrent_ ut failure issue in Unittest
    Signed-off-by: Li <lixingyang13@huawei.com>
    https://gitee.com/openharmony/arkui_napi/issues/I8JFVP

--- native_engine/impl/ark/ark_native_engine.cpp
+++ native_engine/impl/ark/ark_native_engine.cpp
@@ -148,8 +148,9 @@ panda::Local<panda::JSValueRef> NapiDefineClass(napi_env env, const char* name, 
             },
         reinterpret_cast<void*>(funcInfo), true);
     Local<panda::ObjectRef> classPrototype = fn->GetFunctionPrototype(vm);
+    Local<panda::StringRef> fnName = panda::StringRef::NewFromUtf8(vm, className.c_str());
+    fn->SetName(vm, fnName);
     Local<panda::ObjectRef> fnObj = fn->ToObject(vm);
-    reinterpret_cast<ArkNativeEngine*>(env)->SetModuleName(fnObj, className);
     for (size_t i = 0; i < length; i++) {
         if (properties[i].attributes & NATIVE_STATIC) {
             NapiDefineProperty(env, fnObj, properties[i]);
