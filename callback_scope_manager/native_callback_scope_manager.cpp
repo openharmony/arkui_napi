236a09ed2f80c2947312b990a0a51a5cb25a8781
yanpeng <yanpeng51@huawei.com>
2024-01-12 10:21:29 +0800

    实现 napi_open_callback_scope, napi_close_callback_scope 框架
    Signed-off-by: yanpeng <yanpeng51@huawei.com>
    Change-Id: Ib67c87d3dd9eb7da0da6505c8d61bff80f8de0c3

--- callback_scope_manager/native_callback_scope_manager.cpp
+++ callback_scope_manager/native_callback_scope_manager.cpp
@@ -16,9 +16,21 @@
 #include "native_callback_scope_manager.h"
 #include "native_engine/native_engine.h"
 
-NativeCallbackScope::NativeCallbackScope(NativeEngine* env)
+NativeCallbackScope::NativeCallbackScope(NativeEngine* env,
+                                         panda::Local<panda::ObjectRef> object,
+                                         const AsyncIdInfo& asyncIdInfo,
+                                         int flags) : env_(env), asyncIdInfo_(asyncIdInfo),
+    object_(object), skipHooks_(flags & SKIP_ASYNC_HOOKS), skipTaskQueues_(flags & SKIP_TASK_QUEUES)
 {
-    env_ = env;
+    [[maybe_unused]] panda::LocalScope scope(env_->GetEcmaVm());
+
+    // not support async_hook yet, and push_async_context actions need to be improved in the future
+
+    pushedIds_ = true;
+
+    if (asyncIdInfo.asyncId != 0 && !skipHooks_) {
+        NativeAsyncWrap::EmitBefore(env, asyncIdInfo.asyncId);
+    }
 }
 
 NativeCallbackScope::~NativeCallbackScope()
@@ -28,11 +40,38 @@ NativeCallbackScope::~NativeCallbackScope()
 
 void NativeCallbackScope::Close()
 {
-    if (!closed_) {
-        closed_ = true;
+    if (closed_) {
+        return;
+    }
+    closed_ = true;
+
+    auto performStoppingCheck = [this]() {
         if (env_->IsStopping()) {
             MarkAsFailed();
+            // not support async_hook yet, and clear_async_id_stack actions need to be improved in the future
         }
+    };
+    performStoppingCheck();
+
+    if (env_->IsStopping()) {
+        return;
+    }
+
+    if (!failed_ && asyncIdInfo_.asyncId != 0 && !skipHooks_) {
+        NativeAsyncWrap::EmitAfter(env_, asyncIdInfo_.asyncId);
+    }
+
+    if (pushedIds_) {
+        pushedIds_ = false;
+        // not support async_hook yet, and pop_async_context actions need to be improved in the future
+    }
+
+    if (failed_) {
+        return;
+    }
+
+    if (env_->GetCallbackScopeManager()->GetOpenCallbackScopes() > 1 || skipTaskQueues_) {
+        return;
     }
 }
 
@@ -40,9 +79,11 @@ NativeCallbackScopeManager::NativeCallbackScopeManager() {}
 
 NativeCallbackScopeManager::~NativeCallbackScopeManager() {}
 
-NativeCallbackScope* NativeCallbackScopeManager::Open(NativeEngine* env)
+NativeCallbackScope* NativeCallbackScopeManager::Open(NativeEngine* env,
+                                                      panda::Local<panda::ObjectRef> object,
+                                                      AsyncIdInfo asyncIdInfo)
 {
-    NativeCallbackScope* scope = new (std::nothrow)NativeCallbackScope(env);
+    NativeCallbackScope* scope = new (std::nothrow)NativeCallbackScope(env, object, asyncIdInfo);
 
     if (scope) {
         asyncCallbackScopeDepth_++;
