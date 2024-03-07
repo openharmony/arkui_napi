edf7c03f75bec1cc3a4fa051d31547b8e2e3640c
zhanheng <zhanheng2@huawei.com>
2024-03-07 20:49:06 +0800

    kill -39 cpuprofile bugfix
    Signed-off-by: z00522183 <zhanheng2@huawei.com>
    Change-Id: If173299402bc4d22973cec55adb1045bf4f8e300

--- native_engine/native_engine.h
+++ native_engine/native_engine.h
@@ -197,6 +197,8 @@ public:
 
     virtual void DumpHeapSnapshot(const std::string &path, bool isVmMode = true,
         DumpFormat dumpFormat = DumpFormat::JSON) = 0;
+    virtual void DumpCpuProfile(bool isVmMode = true, DumpFormat dumpFormat = DumpFormat::JSON,
+        bool isPrivate = false, bool isFullGC = true) = 0;
     virtual void DumpHeapSnapshot(bool isVmMode = true, DumpFormat dumpFormat = DumpFormat::JSON,
         bool isPrivate = false, bool isFullGC = true) = 0;
     virtual bool BuildNativeAndJsStackTrace(std::string &stackTraceStr) = 0;
