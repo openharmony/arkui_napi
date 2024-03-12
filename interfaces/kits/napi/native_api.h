f97f2ced43a2831ab475930b2de60796627ee072
chenlincl3 <932565568@qq.com>
2024-03-12 19:04:09 +0800

    修改napi_load_module_with_info声明
    Signed-off-by: chenlincl3 <chenlin201@huawei.com>
    Change-Id: Icbe7f4796c62706dccd83639bb99880c069755cb

--- interfaces/kits/napi/native_api.h
+++ interfaces/kits/napi/native_api.h
@@ -79,9 +79,9 @@ NAPI_EXTERN napi_status napi_run_script_path(napi_env env, const char* path, nap
 NAPI_EXTERN napi_status napi_queue_async_work_with_qos(napi_env env, napi_async_work work, napi_qos_t qos);
 NAPI_EXTERN napi_status napi_load_module(napi_env env, const char* path, napi_value* result);
 NAPI_EXTERN napi_status napi_load_module_with_info(napi_env env,
-                                                   napi_value* result,
                                                    const char* path,
-                                                   const char* module_info);
+                                                   const char* module_info,
+                                                   napi_value* result);
 NAPI_EXTERN napi_status napi_get_instance_data(napi_env env, void** data);
 NAPI_EXTERN napi_status napi_set_instance_data(napi_env env,
                                                void* data,
