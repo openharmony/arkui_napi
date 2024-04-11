e9db6b35f42d489114d0d1bc95198f3195ae91b5
shaoyijiang <shaoyijiang@huawei.com>
2024-04-11 20:32:28 +0800

    Modify part dependency and fixed duplicate printing of worker js stack
    Signed-off-by: shaoyijiang <shaoyijiang@huawei.com>
    Change-Id: If5b7678cf509bfbfee8b1d15b3feac071cdf84b1

--- native_engine/impl/ark/ark_native_engine.cpp
+++ native_engine/impl/ark/ark_native_engine.cpp
@@ -2258,6 +2258,7 @@ void HybridStackDumper::BuildJsNativeMixStack(std::vector<JsFrameInfo>& jsFrames
         }
         Write(hybridStackStr);
         Write("\n");
+        return;
     }
 
     std::string nativeStacksStr = Unwinder::GetFramesStr(nativeFrames);
