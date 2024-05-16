87e1267d37c477e338f5af4ffa15bf46a32021f7
EurusHomles-zH <zhouhang65@huawei.com>
2024-05-16 16:46:27 +0800

    Description:JS leakage settting restrictions Signed-off-by: EurusHomles-zH <zhouhang65@huawei.com>

--- native_engine/native_engine.h
+++ native_engine/native_engine.h
@@ -190,6 +190,8 @@ public:
     virtual void* CreateRuntime(bool isLimitedWorker = false) = 0;
     virtual napi_value CreatePromise(NativeDeferred** deferred) = 0;
 
+    virtual void SetJsDumpThresholds(size_t thresholds) = 0;
+
     virtual void StartCpuProfiler(const std::string& fileName = "") = 0;
     virtual void StopCpuProfiler() = 0;
 
@@ -327,9 +329,6 @@ public:
     virtual void SetDebuggerPostTaskFunc(DebuggerPostTask func);
     virtual void CallDebuggerPostTaskFunc(std::function<void()>&& task);
 #endif
-
-    virtual void StartMonitorJSHeapUsage() = 0;
-    virtual void StopMonitorJSHeapUsage() = 0;
 
     virtual void SetHostEngine(NativeEngine* engine);
     virtual NativeEngine* GetHostEngine() const;
