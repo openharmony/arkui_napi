8ba66ce9120dcee3c6798f1d89e7c9c055703cdc
wanglai.yao <yaowanglai@huawei.com>
2024-07-02 11:04:33 +0000

    update native_engine/native_engine.cpp.
    Signed-off-by: wanglai.yao <yaowanglai@huawei.com>

--- native_engine/native_engine.cpp
+++ native_engine/native_engine.cpp
@@ -15,9 +15,6 @@
 
 #include "native_engine/native_engine.h"
 
-#ifdef ENABLE_EVENT_HANDLER
-#include <unistd.h>
-#endif
 #include <uv.h>
 #if !defined(WINDOWS_PLATFORM) && !defined(MAC_PLATFORM) && !defined(IOS_PLATFORM) && !defined(LINUX_PLATFORM)
 #include <sys/epoll.h>
@@ -140,11 +137,6 @@ void NativeEngine::Init()
     tid_ = pthread_self();
     uv_async_init(loop_, &uvAsync_, nullptr);
     uv_sem_init(&uvSem_, 0);
-#ifdef ENABLE_EVENT_HANDLER
-    if ((jsThreadType_ == JSThreadType::MAIN_THREAD) && (getpid() != gettid())) {
-        jsThreadType_ = JSThreadType::DEFAULT_THREAD;
-    }
-#endif
     CreateDefaultFunction();
 }
 
@@ -207,11 +199,6 @@ bool NativeEngine::ReinitUVLoop()
     tid_ = pthread_self();
     uv_async_init(loop_, &uvAsync_, nullptr);
     uv_sem_init(&uvSem_, 0);
-#ifdef ENABLE_EVENT_HANDLER
-    if ((jsThreadType_ == JSThreadType::MAIN_THREAD) && (getpid() != gettid())) {
-        jsThreadType_ = JSThreadType::DEFAULT_THREAD;
-    }
-#endif
     CreateDefaultFunction();
     return true;
 }
