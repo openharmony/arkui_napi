187e6f084b74e8fdf69eccbead2ba849db973804
wangbowen36 <wangbowen36@huawei.com>
2024-08-01 16:16:46 +0800

    fix jsvm coverage build
    Signed-off-by: wangbowen36 <wangbowen36@huawei.com>

--- jsvm/build_jsvm_inter.sh
+++ jsvm/build_jsvm_inter.sh
@@ -153,6 +153,10 @@ do_env() {
         die "not support target cpu"
     fi
 
+    if [[ "${TARGET_CLANG_COVERAGE}" = "true" ]]; then
+        cflags+=" --coverage"
+    fi
+
     cflags+=" ${argurment}"
 
     # linux host env
