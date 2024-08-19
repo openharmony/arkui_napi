01237822002ef52dd2501bad2669fcaa3b626870
dongbosi@huawei.com <dongbosi@huawei.com>
2024-08-19 19:07:35 +0800

    add new function GetIsDebugModeEnabled
    Signed-off-by: dongbosi@huawei.com <dongbosi@huawei.com>

--- native_engine/native_engine.h
+++ native_engine/native_engine.h
@@ -325,7 +325,7 @@ public:
         std::lock_guard<std::mutex> alivedEngLock(g_alivedEngineMutex_);
         return g_alivedEngine_.find(env) != g_alivedEngine_.end();
     }
-    
+
     inline void SetUnalived()
     {
         std::lock_guard<std::mutex> alivedEngLock(g_alivedEngineMutex_);
@@ -394,6 +394,7 @@ public:
     virtual void GetCurrentModuleInfo(std::string& moduleName, std::string& fileName, bool needRecordName) = 0;
     virtual bool GetIsBundle() = 0;
     virtual bool GetIsNormalizedOhmUrlPack() = 0;
+    virtual bool GetIsDebugModeEnabled() = 0;
     virtual std::string GetBundleName() = 0;
     virtual bool IsExecuteModuleInAbcFile(std::string bundleName, std::string moduleName, std::string ohmurl) = 0;
     virtual int GetProcessStartRealTime() = 0;
