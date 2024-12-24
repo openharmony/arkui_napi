3bce1ab8fd27407bd237299823ca32fa865542fe
tuxiaohang <tuxiaohang@huawei.com>
2024-12-24 11:09:22 +0800

    Revert "修改napi_has_own_property的实现，只检查对象自身是否具有指定名称的属性。"
    This reverts commit 85ad6cddcb22cf524f1266e9ea11eb4214a57ea5.
    
    Signed-off-by: tuxiaohang <tuxiaohang@huawei.com>
    Change-Id: I69335a77ae76ce2f2dec39a94976f39c5d6142d1

--- native_engine/native_engine.h
+++ native_engine/native_engine.h
@@ -325,7 +325,6 @@ public:
     virtual NativeEngine* GetHostEngine() const;
     virtual void SetApiVersion(int32_t apiVersion);
     virtual int32_t GetApiVersion();
-    virtual int32_t GetRealApiVersion() const;
     virtual bool IsApplicationApiVersionAPI11Plus();
 
     virtual napi_status AddCleanupHook(CleanupCallback fun, void* arg);
@@ -588,7 +587,6 @@ private:
     uv_sem_t uvSem_;
     // Application's sdk version
     int32_t apiVersion_ = 8;
-    int32_t realApiVersion_ = 8;
 
     // the old worker api use before api9, the new worker api start with api9
     enum JSThreadType { MAIN_THREAD, WORKER_THREAD, TASKPOOL_THREAD, RESTRICTEDWORKER_THREAD, NATIVE_THREAD };
