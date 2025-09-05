b87dfb8fbff1f4317b475ed8de3c39235a1b1f97
wangbowen36 <wangbowen36@h-partners.com>
2025-09-05 17:06:11 +0800

    FinalizersCallbackTest001用例下线
    Issue: https://gitee.com/openharmony/arkui_napi/issues/ICWM8X
    Signed-off-by: wangbowen36 <wangbowen36@h-partners.com>

--- test/unittest/test_napi_context.cpp
+++ test/unittest/test_napi_context.cpp
@@ -1212,40 +1212,6 @@ HWTEST_F(NapiContextTest, NapiEnvCallbackScopeManagerTest002, testing::ext::Test
     ASSERT_NE(engine_->GetCallbackScopeManager(), nullptr);
 }
 
-/**
- * @tc.name: FinalizersCallbackTest001
- * @tc.desc: Test finalize callback execution of napi_wrap
- * @tc.type: FUNC
- */
-HWTEST_F(NapiContextTest, FinalizersCallbackTest001, testing::ext::TestSize.Level0)
-{
-    CheckContextEnv();
-    ASSERT_NE(multiContextEngine_, nullptr);
-    napi_env env = reinterpret_cast<napi_env>(multiContextEngine_);
-    const EcmaVM *vm = reinterpret_cast<ArkNativeEngine*>(engine_)->GetEcmaVm();
-
-    const char *str = GetTestCaseName();
-    size_t size = 2 * ArkNativeEngine::FINALIZERS_PACK_PENDING_NATIVE_BINDING_SIZE_THRESHOLD;
-    static bool finalizersCallbackDone[2] = {false, false};
-
-    for (int i = 0; i < 2; ++i) {
-        {
-            panda::LocalScope scope(vm);
-            napi_value object = nullptr;
-            napi_create_object(env, &object);
-            napi_wrap_with_size(env, object, (void*)str, [](napi_env env, void *data, void *hint) {
-                bool *result = reinterpret_cast<bool*>(hint);
-                ASSERT_FALSE(*result);
-                *result = true;
-            }, reinterpret_cast<void*>(&finalizersCallbackDone[i]), nullptr, size);
-        }
-        panda::JSNApi::TriggerGC(vm, panda::ecmascript::GCReason::OTHER, panda::JSNApi::TRIGGER_GC_TYPE::FULL_GC);
-    }
-
-    ASSERT_FALSE(finalizersCallbackDone[0]);
-    ASSERT_TRUE(finalizersCallbackDone[1]);
-}
-
 class ContextAsyncWorkTestData final : public NapiAsyncWorkTestData {
 public:
     ContextAsyncWorkTestData(napi_env env, const char* name) : NapiAsyncWorkTestData(env, name) {}
