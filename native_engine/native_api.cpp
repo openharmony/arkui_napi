17cdb241bc4b6f5f76a37d1d700acee62ecc7ba3
tuxiaohang <tuxiaohang@huawei.com>
2025-09-05 16:56:04 +0800

    change to debug log
    Issue: https://gitee.com/openharmony/arkui_napi/issues/ICWMGB
    Signed-off-by: tuxiaohang <tuxiaohang@huawei.com>
    Change-Id: Ia48ffa1377eb8622bac155773e5989e82add2b09

--- native_engine/native_api.cpp
+++ native_engine/native_api.cpp
@@ -1911,7 +1911,7 @@ NAPI_EXTERN napi_status napi_wrap(napi_env env,
     auto reference = reinterpret_cast<NativeReference**>(result);
     Local<panda::StringRef> key = panda::StringRef::GetNapiWrapperString(vm);
     if (nativeObject->Has(vm, key)) {
-        HILOG_WARN("napi_wrap: current js_object has been wrapped.");
+        HILOG_DEBUG("napi_wrap: current js_object has been wrapped.");
     }
     Local<panda::ObjectRef> object = panda::ObjectRef::New(vm);
     NativeReference* ref = nullptr;
@@ -2050,7 +2050,7 @@ NAPI_EXTERN napi_status napi_wrap_async_finalizer(napi_env env,
     auto reference = reinterpret_cast<NativeReference**>(result);
     Local<panda::StringRef> key = panda::StringRef::GetNapiWrapperString(vm);
     if (nativeObject->Has(vm, key)) {
-        HILOG_WARN("napi_wrap_async_finalizer: current js_object has been wrapped.");
+        HILOG_DEBUG("napi_wrap_async_finalizer: current js_object has been wrapped.");
     }
     Local<panda::ObjectRef> object = panda::ObjectRef::New(vm);
     NativeReference* ref = nullptr;
@@ -2089,7 +2089,7 @@ NAPI_EXTERN napi_status napi_wrap_with_size(napi_env env,
     auto reference = reinterpret_cast<NativeReference**>(result);
     Local<panda::StringRef> key = panda::StringRef::GetNapiWrapperString(vm);
     if (nativeObject->Has(vm, key)) {
-        HILOG_WARN("napi_wrap_with_size: current js_object has been wrapped.");
+        HILOG_DEBUG("napi_wrap_with_size: current js_object has been wrapped.");
     }
     Local<panda::ObjectRef> object = panda::ObjectRef::New(vm);
     NativeReference* ref = nullptr;
