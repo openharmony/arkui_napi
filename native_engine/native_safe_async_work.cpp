944e6fe06a912efee7e79a732e370476454a3513
ohci1 <ohci1@noreply.gitcode.com>
2025-09-26 09:27:14 +0800

    napi异步队列支持高优任务插队处理
    Signed-off-by: ningqicheng <ningqicheng@huawei.com>
    Change-Id: Iddae6d2467cb7f521f8fe2b55963b5614018c80a
    
    
    revert merge request: napi异步队列支持高优任务插队处理
    
    8a1e949f026b331bcca8df849dde69e58e35ccac
    
    This revert merge request !2388

--- native_engine/native_safe_async_work.cpp
+++ native_engine/native_safe_async_work.cpp
@@ -344,9 +344,7 @@ void NativeSafeAsyncWork::ProcessAsyncHandle()
     } else {
         loop = engine_->GetParent()->GetUVLoop();
     }
-    lock.unlock();
     uv_call_specify_task(loop);
-    lock.lock();
 #endif
     while (size > 0) {
         data = queue_.front();
