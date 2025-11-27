6279448e310f9fed47d3bd6943415a182d4a51e0
quguiren <quguiren1@huawei-partners.com>
2025-11-27 14:42:22 +0800

    fix android load abc repeatedly
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

--- module_manager/native_module_manager.cpp
+++ module_manager/native_module_manager.cpp
@@ -682,6 +682,10 @@ NativeModule* NativeModuleManager::LoadNativeModule(const char* moduleName, cons
     }
     NativeModule* nativeModule =
         FindNativeModuleByCache(strModule.c_str(), nativeModulePath, cacheNativeModule, cacheHeadTailNativeModule);
+    if (nativeModule == nullptr) {
+        nativeModule =
+            FindNativeModuleByCache(strCutName.c_str(), nativeModulePath, cacheNativeModule, cacheHeadTailNativeModule);
+    }
 #else
     std::string key(moduleName);
     if (isAppModule) {
@@ -712,6 +716,10 @@ NativeModule* NativeModuleManager::LoadNativeModule(const char* moduleName, cons
 #ifdef ANDROID_PLATFORM
             nativeModule = FindNativeModuleByCache(strModule.c_str(), nativeModulePath, cacheNativeModule,
                                                    cacheHeadTailNativeModule);
+            if (nativeModule == nullptr) {
+                nativeModule = FindNativeModuleByCache(strCutName.c_str(), nativeModulePath, cacheNativeModule,
+                    cacheHeadTailNativeModule);
+            }
 #else
             nativeModule =
                 FindNativeModuleByCache(key.c_str(), nativeModulePath, cacheNativeModule, cacheHeadTailNativeModule);
