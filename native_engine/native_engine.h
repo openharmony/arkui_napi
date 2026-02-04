512f7a2227810cd6bbefc6147f1bac85b9ffdd5b
w30079292 <wangshengmao@h-partners.com>
2026-02-04 15:56:39 +0800

    [Bug]: add serialize js stack in taskpool
    Issue: https://gitcode.com/openharmony/commonlibrary_ets_utils/issues/2283
    Signed-off-by: w30079292 <wangshengmao@h-partners.com>
    Change-Id: I4a07d11556dedac2d7b2e261f0a615b7b2e69fe6

--- native_engine/native_engine.h
+++ native_engine/native_engine.h
@@ -290,6 +290,9 @@ public:
     virtual void GetHybridStackTraceForCrash(napi_env env, std::string &stackTraceStr) = 0;
     virtual void SerializeJSError(napi_env env, napi_value object, bool defaultTransfer,
                                   bool defaultCloneSendable, void** result) = 0;
+    virtual void SerializeJSErrorWithError(napi_env env, napi_value object, bool defaultTransfer,
+                                           bool defaultCloneSendable, void** result,
+                                           std::string& error) = 0;
     virtual bool DeleteWorker(NativeEngine* workerEngine) = 0;
     virtual bool StartHeapTracking(double timeInterval, bool isVmMode = true) = 0;
     virtual bool StopHeapTracking(const std::string &filePath) = 0;
